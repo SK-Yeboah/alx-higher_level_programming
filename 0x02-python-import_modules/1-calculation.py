0b9f69c1cc78f8ebdcf7aaf43fb27995ecdf4c90
SK-Yeboah <skyeboah9@gmail.com>
2023-11-04 17:22:06 -0700

    calculation

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     """Print the sum, dif , mul and quot of 10 and 5"""
-    from  calculator_1 import add , subtract , multiply , divide
+    from  calculator_1 import add , sub , mul , div
 
     a = 10
     b = 5
