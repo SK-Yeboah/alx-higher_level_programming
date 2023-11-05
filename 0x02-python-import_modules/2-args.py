a57770379f87f158ab6c102e7daf5ebf27ff1605
SK-Yeboah <skyeboah9@gmail.com>
2023-11-04 17:49:57 -0700

    argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Print the list of Arguments """
+
+    import sys
+
+   arg_num = len(sys.argv) - 1
+
+   if arg_num == 0:
+       print("0 argument.")
+   elif arg_num  == 1:
+       print(" 1 argument")
+    
+    for i in  range (arg_num)
+    print("{}: {}".format(i+1, sys.argv[i+1]))
