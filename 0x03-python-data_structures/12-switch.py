e96e1e7f761ec0d599b4f2e6cc6d7f82eb79f766
SK-Yeboah <skyeboah9@gmail.com>
2023-11-06 03:53:20 -0800

    switch

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-    new_list = my_list[:idx] + my_list[idx+1:]
-    return new_list
+a = 89
+b = 10
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+a,b = b , a
+print("a={:d} - b={:d}".format(a, b))
