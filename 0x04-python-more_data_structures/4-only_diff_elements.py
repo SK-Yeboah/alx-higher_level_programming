9e6fb6501f93a7c82c962dcafca08e1fc8d426cc
SK-Yeboah <skyeboah9@gmail.com>
2023-11-10 07:07:26 -0800

    set1 and set2

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    common_set = set_1 & set_2
-    return common_set
+    # Create a new set to store elements present in only one set
+    diff_set = set()
+
+    # Iterate through elements in set_1
+    for element in set_1:
+        # Check if the element is not present in set_2
+        if element not in set_2:
+            # Add the element to the diff_set
+            diff_set.add(element)
+
+    # Iterate through elements in set_2
+    for element in set_2:
+        # Check if the element is not present in set_1
+        if element not in set_1:
+            # Add the element to the diff_set
+            diff_set.add(element)
+
+    return diff_set
