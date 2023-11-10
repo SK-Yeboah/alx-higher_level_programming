4a7a681685f70e9df6e2935b4f8d451f547b1a74
SK-Yeboah <skyeboah9@gmail.com>
2023-11-10 08:24:27 -0800

    best_score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+def best_score(a_dictionary):
+    # Check if the dictionary is empty
+    if not a_dictionary:
+        return None
+
+    # Find the key with the maximum value using max()
+    best_key = max(a_dictionary, key=a_dictionary.get)
+
+    return best_key
