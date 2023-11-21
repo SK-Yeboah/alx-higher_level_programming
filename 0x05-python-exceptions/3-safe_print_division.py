8d036ab9c86f2ff996e42269a6af81b9f4e0d056
SK-Yeboah <skyeboah9@gmail.com>
2023-11-21 06:22:54 -0800

    print

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,15 @@
+#!usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = (a / b)
+    except ZeroDivisionError:
+        print("Division is not allowed")
+        return None
+    except Exception as e:
+        print(f"Am error occured at: {e}")
+        return None
+    else:
+        print("Inside result: {}".format(result))
+        return result
+    finally:
+        print("Finally: Division operation has completed")
