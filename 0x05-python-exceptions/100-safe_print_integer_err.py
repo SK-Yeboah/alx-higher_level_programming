8731bcfc560d4d332c0c6d8140c0bf47bc9dcfab
SK-Yeboah <skyeboah9@gmail.com>
2023-11-21 07:22:08 -0800

    advance print

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,7 +2,7 @@
 import sys
 def safe_print_integer_err(value):
     try:
-        print("Value is {}".format(int(value)));
+        print("Value is {:d}".format(value));
         return True
     except Exception as e:
         print("Exception: {}".format(e), file =sys.stderr)
