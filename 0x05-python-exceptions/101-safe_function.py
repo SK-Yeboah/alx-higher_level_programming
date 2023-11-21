c6afb52ee2cca4edaea2997cf62f6579274fe1ee
SK-Yeboah <skyeboah9@gmail.com>
2023-11-21 07:34:42 -0800

    fct

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,6 +3,9 @@ def safe_function(fct, *args):
     try:
         result = fct(*args)
         return result
+    except ZeroDivisionError:
+        print("Exception: Division by zero", file=sys.stderr)
+        return None
     except Exception as e:
         print(f"Exception: {e}", file=sys.stderr)
         return None
