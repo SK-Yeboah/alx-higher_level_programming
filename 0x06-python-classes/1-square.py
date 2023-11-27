69d9c91b82d93d1488f93b37f89c6c7981e4d98c
SK-Yeboah <skyeboah9@gmail.com>
2023-11-27 17:37:40 +0000

    instantiationg

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Define a square """
+class Square:
+    def __init__(self, size):
+
+        """Instantiating new intance of Square class"""
+        self.__size__ = size
+   
