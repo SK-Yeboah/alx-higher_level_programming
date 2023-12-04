902818526fe75c61d8f58b75ee5d20c20fd5e41d
SK-Yeboah <skyeboah9@gmail.com>
2023-12-04 14:07:04 -0800

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Defines an object attribute lookup function."""
+
+def lookup(obj):
+    """Return a list of an object's available attributes."""
+    return [attribute for attribute in dir(obj) if not attribute.startswith("__")]
