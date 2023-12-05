6cc81bb88a9f491b4e7329c19cdaf78661d56bab
SK-Yeboah <skyeboah9@gmail.com>
2023-12-05 01:14:12 -0800

    inherits

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Defines an inherited class-checking function."""
 def inherits_from(obj, a_class):
     """
     Check if the object is an instance of a class that inherited (directly or indirectly)
