efde134ce60b3a3e90460b416428c2728b195270
SK-Yeboah <skyeboah9@gmail.com>
2023-12-06 00:59:03 -0800

    3-json

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+"""Define file to save to json"""
 import json
-"""Defile save json file"""
+
 def save_to_json_file(my_obj, filename):
     """Write an object to a text file using json representation"""
     with open(filename, 'w') as file:
