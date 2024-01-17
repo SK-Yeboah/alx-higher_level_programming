968bd882775ef9cbdc93f44c2230e74cc29c5956
SK-Yeboah <skyeboah9@gmail.com>
2024-01-17 19:48:01 +0000

    SQL-MORE QUARIES

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,4 +6,9 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 
 -- create table
-CREATE TABLE IF NOT EXISTS cities (id INT AUTO_INCREMENT PRIMARY KEY, state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES states(id));
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    state_id INT NOT NULL, 
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY(state_id) REFERENCES states(id)
+    );
