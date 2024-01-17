6ca372b6f68f12a243febb7bc5eb5a5db61241cb
SK-Yeboah <skyeboah9@gmail.com>
2024-01-17 20:02:20 +0000

    L_JOIN

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Select all cities with associated state names
 SELECT cities.id, cities.name, states.name 
 FROM cities 
-JOIN states ON cities.id = cities.state_id 
+LEFT JOIN states ON cities.id = cities.state_id 
 ORDER BY cities.id;
