faa9a1d8b52f45e85e2eb39c3519c84f8eaf1b8a
SK-Yeboah <skyeboah9@gmail.com>
2024-01-17 23:11:20 +0000

    Join

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- List genres and the number of shows linked to each genre
+SELECT tv_show_genres.genre_id AS genre, COUNT(tv_shows.id) AS number_of_shows
+FROM tv_show_genres
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_of_shows DESC;
