6f7fdaa08ae3f3bd5079554b349623e6191bde95
SK-Yeboah <skyeboah9@gmail.com>
2024-01-17 20:37:50 +0000

    L_JOIN

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- Select shows with linked genres
-SELECT tv_shows.title, tv_show_genres.genres_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 from tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
