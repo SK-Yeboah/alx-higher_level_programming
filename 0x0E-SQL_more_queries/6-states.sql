d32ed765f9c6b9507deef5d0a40661756160a5c5
SK-Yeboah <skyeboah9@gmail.com>
2024-01-17 18:59:58 +0000

    SQL-CREATE

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,6 +2,6 @@
 -- Create the database only if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- Switch to the specified database
-USE DATABASE hbtn_0d_usa
+USE hbtn_0d_usa
 -- Create the table only if it doesn't exist
 CREATE TABLE IF NOT EXISTS states( id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
