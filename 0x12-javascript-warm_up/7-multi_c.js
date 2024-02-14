054447ae05833805d9990494fb215b8cdc6de4a2
SK-Yeboah <skyeboah9@gmail.com>
2024-02-14 10:09:27 -1200

    multi-city

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-const argument = process.argv[2];
-const x = parseInt(argument);
-if(!isNaN(x)){
-    console.log("Missing number of occurance");
-}else{
-    for(let i = 0; i< x; i++){
-        console.log("C is fun");
-    }
+const x = Math.floor(Number(process.argv[2]));
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
