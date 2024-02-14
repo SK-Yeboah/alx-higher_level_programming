363a67914420408b8465c5fbbee799a414aaca9a
SK-Yeboah <skyeboah9@gmail.com>
2024-02-14 09:49:41 -1200

    new line

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
-const argCount = process.argv.length - 2
-
-console.log(argCount)
-
+const argCount = process.argv.length - 2;
 if(argCount === 0){
-    console.log("No argument found")
+    console.log("No argument found");
 }else if (argCount == 1){
-    console.log("Argument found")
+    console.log("Argument found");
 }else{
-    console.log("Argument found")
+    console.log("Argument found");
 } 
