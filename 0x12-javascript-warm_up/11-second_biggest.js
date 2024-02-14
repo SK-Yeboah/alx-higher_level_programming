ed44d1d0ba3fa9afc0734da24129a3f61a2193bf
SK-Yeboah <skyeboah9@gmail.com>
2024-02-14 10:16:32 -1200

    second

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,21 @@
 #!/usr/bin/node
-const myObject = {
-  type: 'object',
-  value: 12
-};
-console.log(myObject);
-myObject.value = 89;
-console.log(myObject);
+
+// Accessing command line arguments using process.argv
+const args = process.argv.slice(2); // Exclude 'node' and the script file name
+
+// Check the number of arguments
+const numArgs = args.length;
+
+// Check if no arguments are passed or only one argument is passed
+if (numArgs === 0 || numArgs === 1) {
+  console.log(0);
+} else {
+  // Convert arguments to integers and sort them in descending order
+  const sortedIntegers = args.map(Number).sort((a, b) => b - a);
+
+  // Find the second biggest integer
+  const secondBiggest = sortedIntegers[1];
+
+  // Print the result
+  console.log(secondBiggest);
+}
