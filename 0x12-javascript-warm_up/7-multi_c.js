9a06f8b8d5aa8be501ecda5c1a203bab9764a063
SK-Yeboah <skyeboah9@gmail.com>
2024-02-14 10:04:05 -1200

    fixed2

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-const argument = process.argv[2]
-
-const x = parseInt(argument)
+const argument = process.argv[2];
+const x = parseInt(argument);
 if(!isNaN(x)){
-    console.log("Missing number of occurance")
+    console.log("Missing number of occurance");
 }else{
     for(let i = 0; i< x; i++){
-        console.log("C is fun")
+        console.log("C is fun");
     }
 }
