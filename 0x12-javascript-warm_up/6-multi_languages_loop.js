8a64063f95cc18b6e5171f275fadc858da85e66e
SK-Yeboah <skyeboah9@gmail.com>
2024-02-14 09:58:33 -1200

    fixed

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-for(const line of ["C is fund", "Python is fun", "Javascript is amazing"]){
+for(const line of ['C is fund', 'Python is fun', 'Javascript is amazing']){
     console.log(line);
 }
