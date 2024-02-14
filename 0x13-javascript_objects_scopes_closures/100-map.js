e826309d18b54201f6cb5b123e4c0580f408cc8f
SK-Yeboah <skyeboah9@gmail.com>
2024-02-14 10:53:46 -1200

    print_1

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-
-export function converter (base) {
-    return function (number) {
-        return number.toString(base);
-    };
-}
+const list = require('./100-data.js').list;
+console.log(list);
+console.log(list.map((item, index) => item * index));
--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,35 +1,6 @@
 #!/usr/bin/node
+const fs = require('fs');
 
-import { readFile, writeFile } from 'fs';
-
-// Extract command line arguments
-const [, , sourceFilePath1, sourceFilePath2, destinationFilePath] = process.argv;
-
-// Read content from the first source file
-readFile(sourceFilePath1, 'utf8', (err, data1) => {
-    if (err) {
-        console.log(`Error reading file ${sourceFilePath1}: ${err.message}`);
-        return;
-    }
-
-    // Read content from the second source file
-    readFile(sourceFilePath2, 'utf8', (err, data2) => {
-        if (err) {
-            console.log(`Error reading file ${sourceFilePath2}: ${err.message}`);
-            return;
-        }
-
-        // Concatenate the content of both files
-        const concatenatedContent = data1 + data2;
-
-        // Write the concatenated content to the destination file
-        writeFile(destinationFilePath, concatenatedContent, 'utf8', (err) => {
-            if (err) {
-                console.log(`Error writing to file ${destinationFilePath}: ${err.message}`);
-                return;
-            }
-
-            console.log(`Files ${sourceFilePath1} and ${sourceFilePath2} concatenated successfully to ${destinationFilePath}.`);
-        });
-    });
-});
+const fArg = fs.readFileSync(process.argv[2]).toString();
+const sArg = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], fArg + sArg);
