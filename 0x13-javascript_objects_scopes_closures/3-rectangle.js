54832c4cfa99211a7f8a2a3a98d4c1c16d7caba2
SK-Yeboah <skyeboah9@gmail.com>
2024-02-14 10:40:30 -1200

    print

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,23 +1,21 @@
 #!/usr/bin/node
-class Rectangle{
-    
-    constructor(w, h) {
-        if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-          return {}; // Return an empty object if w or h is 0 or not a positive integer
-        } else {
-          this.width = w;
-          this.height = h;
-        }
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
     }
+  }
 
-    print() {
-      if (Object.keys(this).length === 0) {
-        console.log("Empty object");
-      } else {
-        for (let i = 0; i < this.height; i++) {
-          console.log("X".repeat(this.width));
-        }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
       }
+      console.log(s);
     }
-    
+  }
 }
+
+module.exports = Rectangle;
