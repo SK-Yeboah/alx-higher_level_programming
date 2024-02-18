b6879d9f6f8980ceef5de805e23df7575b3ff594
SK-Yeboah <skyeboah9@gmail.com>
2024-02-18 10:00:57 -1200

    7-mdal

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,18 +1,35 @@
 #!/usr/bin/python3
-"""Start link class to table in database
-"""
+"""Start link class to table in database"""
 import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
+if __name__ == '__main__':
+    if len(sys.argv)  != 4:
+        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
+        sys.exit(1)
+    
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    # Database connection string
+    db_connection = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine(db_connection.format(username, password, database), pool_pre_ping=True)
+
+    # Create a sessionmaker
     Session = sessionmaker(bind=engine)
+
+    # Create a session
     session = Session()
-    for instance in session.query(State).order_by(State.id):
-        print(instance.id, instance.name, sep=": ")
-        
+
+    #Query all state objects and sort them by id
+    states = session.query(state).order_by(State.id).all()
+
+    #Display results
+    for state in states:
+        print("{}:{}".format(state.id, state.name))
+    
+
+    #Close the session
+    session.close()
