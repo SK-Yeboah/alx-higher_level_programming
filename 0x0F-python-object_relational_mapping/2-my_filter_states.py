70f7210eeafe99e13752839be1c9eabfb9279ac9
SK-Yeboah <skyeboah9@gmail.com>
2024-02-18 09:42:58 -1200

    2-filter

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,39 +1,17 @@
 #!/usr/bin/python3
-import sys
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
+import sys
 
-if __name__== "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
-        sys.exit(1)
 
-    username, password, database, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
-
-    """Connecting To MySQL Server"""
-    try:   
-        db = MySQLdb.connect(host="localhost", user =username, passwd=password, db = database, port=33306)
-    except MySQLdb.Error as e:
-        print("Error connecting to the MySQL:{}".format(e))
-
-    """Creating  A Cursor Object"""
-    cursor = db.cursor()
-
-    """Query To Find All States from hbtn_0e_0_use"""
-    query = "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC"
-
-    """Executing The Query"""
-    try:
-        cursor.execute(query, (state_name,))
-    except MySQLdb.Error as e:
-        print("Error executing query: {}".format())
-        db.close()
-        sys.exit(1)
-
-    """Fetch All And Display The Result"""
-    result = cursor.fetchall()
-    for row in result:
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id""")
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
-
-    """Close the cursor and database connection"""
-    cursor.close()
+    cur.close()
     db.close()
