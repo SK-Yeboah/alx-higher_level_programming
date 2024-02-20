532c6994f63de3203e5cdd49e7bf6e15ed0f9f1a
SK-Yeboah <skyeboah9@gmail.com>
2024-02-20 08:45:49 -1200

    model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-"""Module containing the definition of the State Class"""
+"""Define State class and create Base object."""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-"""Creating an instance of the declaratiive_base"""
 Base = declarative_base()
 
-"""Define the state class"""
 class State(Base):
-    """Class representing a state"""
-    __tablename = 'state'
-    id = Column(Integer, primary_key = True, nullable=True, autoincrement=True)
+    """Define State class."""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
