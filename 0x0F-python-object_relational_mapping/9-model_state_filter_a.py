d3f8aa35c8ab8cc2fa06f01074db241b3ea6795c
SK-Yeboah <skyeboah9@gmail.com>
2024-02-20 08:55:14 -1200

    9-model

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
+"""Lists all State objects that contain the letter 'a' from the database hbtn_0e_6_usa."""
 
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-
-if __name__ == '__main__':
-    if len(sys.argv)  != 4:
+if __name__ == "__main__":
+    # Check if the correct number of command-line arguments is provided
+    if len(sys.argv) != 4:
         print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
         sys.exit(1)
-    
+
+    # Extract MySQL username, password, and database name from command-line arguments
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
     # Database connection string
     db_connection = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
     engine = create_engine(db_connection.format(username, password, database), pool_pre_ping=True)
 
-    # Create a sessionmaker
+    # Create session maker
     Session = sessionmaker(bind=engine)
 
     # Create a session
@@ -29,7 +31,6 @@ if __name__ == '__main__':
     # Display the results
     for state in states_with_a:
         print("{}: {}".format(state.id, state.name))
-    
 
-    #Close the session
+    # Close the session
     session.close()
