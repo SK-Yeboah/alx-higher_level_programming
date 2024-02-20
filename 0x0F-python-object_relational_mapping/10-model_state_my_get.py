5d05ce80d72e8c09fb19bdbf0ee2c869c14e8cc1
SK-Yeboah <skyeboah9@gmail.com>
2024-02-20 08:56:31 -1200

    10-model

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
+"""Prints the State object with the name passed as an argument from the database hbtn_0e_6_usa."""
 
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-
-if __name__ == '__main__':
-    if len(sys.argv)  != 5:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
+if __name__ == "__main__":
+    # Check if the correct number of command-line arguments is provided
+    if len(sys.argv) != 5:
+        print("Usage: {} <mysql_username> <mysql_password> <database_name> <state_name>".format(sys.argv[0]))
         sys.exit(1)
-    
+
+    # Extract MySQL username, password, database name, and state name from command-line arguments
     username, password, database, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
 
     # Database connection string
     db_connection = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
     engine = create_engine(db_connection.format(username, password, database), pool_pre_ping=True)
 
-    # Create a sessionmaker
+    # Create session maker
     Session = sessionmaker(bind=engine)
 
     # Create a session
     session = Session()
 
-    # Query State objects containing the letter 'a'
+    # Query the State object with the specified name
     state = session.query(State).filter(State.name == state_name).first()
 
     # Display the result
@@ -31,6 +33,6 @@ if __name__ == '__main__':
         print(state.id)
     else:
         print("Not found")
-        
-    #Close the session
+
+    # Close the session
     session.close()
