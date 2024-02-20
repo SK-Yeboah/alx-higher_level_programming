16baf04c2b85ba54d319e5de494d977d3cbb2145
SK-Yeboah <skyeboah9@gmail.com>
2024-02-20 08:37:27 -1200

    new

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,40 +1,45 @@
 #!/usr/bin/python3
 """  lists all states from the database hbtn_0e_0_usa """
-import sys
 import MySQLdb
+import sys
 
-if __name__== "__main__":
-    if len(sys.argv) != 5:
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
         print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
         sys.exit(1)
 
-    username, password, database, state_name= sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    """Connecting To MySQL Server"""
-    try:   
-        db = MySQLdb.connect(host="localhost", user =username, passwd=password, db = database, port=33306)
+    try:
+        # Connect to MySQL server
+        db = MySQLdb.connect(
+            host="localhost",
+            user=username,
+            passwd=password,
+            db=database,
+            port=3306
+        )
     except MySQLdb.Error as e:
-        print("Error connecting to the MySQL:{}".format(e))
+        print("Error connecting to MySQL: {}".format(e))
+        sys.exit(1)
 
-    """Creating  A Cursor Object"""
+    # Create a cursor object
     cursor = db.cursor()
 
-    """Query To Find All States from hbtn_0e_0_use"""
-    query = "SELECT * FROM cities WHERE name = %s ORDER BY cities.id ASC"
-
-    """Executing The Query"""
+    # Execute the query
+    query = "SELECT * FROM cities ORDER BY cities.id ASC"
     try:
-        cursor.execute(query, (state_name,))
+        cursor.execute(query)
     except MySQLdb.Error as e:
-        print("Error executing query: {}".format())
+        print("Error executing query: {}".format(e))
         db.close()
         sys.exit(1)
 
-    """Fetch All And Display The Result"""
-    result = cursor.fetchall()
-    for row in result:
+    # Fetch and display the results
+    results = cursor.fetchall()
+    for row in results:
         print(row)
 
-    """Close the cursor and database connection"""
+    # Close the cursor and database connection
     cursor.close()
     db.close()
