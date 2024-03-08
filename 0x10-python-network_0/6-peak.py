167e9a8d0a039029817ebb56c785e95d6decc4fe
SK-Yeboah <skyeboah9@gmail.com>
2024-03-08 13:58:45 -0800

    peak_1

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,9 +2,7 @@
 """Find a peak in a list of unsorted integers"""
 
 def find_peak(list_of_integers):
-     """
-    Find a peak in a list of unsorted integers.
-    """
+    """Find a peak in a list of integers."""
     if not list_of_integers:
         return None
     
