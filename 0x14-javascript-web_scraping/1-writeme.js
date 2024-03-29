5920e91ffabddaed094fb817b55c17169c9bd379
SK-Yeboah <skyeboah9@gmail.com>
2024-03-29 04:49:36 -1200

    write to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+
+if (process.argv.length !== 4) {
+  console.error('Usage: node 1-writeme.js <file_path> "<string_to_write>"');
+  process.exit(1); // Exit with error
+}
+
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+
+
+fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
+  if (err) {
+    console.error(err); 
+    return;
+  }
+  
+  console.log(`Content successfully written to ${filePath}`); 
+});
