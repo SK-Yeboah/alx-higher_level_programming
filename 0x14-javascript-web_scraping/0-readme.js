7bd3ee5aa413bb9375322cd6bb47118135c42470
SK-Yeboah <skyeboah9@gmail.com>
2024-03-29 04:20:11 -1200

    read_File

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,9 +2,15 @@
 const fs  = require('fs')
 const filepath = process.argv[2]
 
+
 fs.readFile(filepath,'utf-8', (err,data) =>{
     if(err){
-        console.err(err)
+        if(err.code === 'ENOENT'){
+            console.error(`{Error: ${err.code}: no such  firle or directory} `);
+        }else{
+            console.error(err)
+        }
+        
         return
     }
     console.log(data)
