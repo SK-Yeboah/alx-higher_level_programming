aff846256a39bc94d3b6cd722b30a555aa55f4be
SK-Yeboah <skyeboah9@gmail.com>
2024-03-29 05:35:21 -1200

    write to a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const fs  = require('fs')
-const filepath = process.argv[2]
+const fs =require('fs');
+const filepath=process.argv[2];
 fs.readFile(filepath,'utf-8', (err,data)=>{
-    console.log(data || err)
+    console.log(data || err);
 });
