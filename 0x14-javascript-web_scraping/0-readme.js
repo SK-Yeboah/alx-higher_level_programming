5092a40bce25863e9a41ebeeb34a3da015ec96bf
SK-Yeboah <skyeboah9@gmail.com>
2024-03-29 04:28:23 -1200

    read_File+_!

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,14 +4,6 @@ const filepath = process.argv[2]
 
 
 fs.readFile(filepath,'utf-8', (err,data) =>{
-    if(err){
-        if(err.code === 'ENOENT'){
-            console.error(`{Error: ${err.code}: no such  firle or directory} `);
-        }else{
-            console.error(err)
-        }
-        
-        return
-    }
-    console.log(data)
+   
+    console.log(data || err)
 });
