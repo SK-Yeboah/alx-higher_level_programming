e0bb70090aa9ff923962022878dab6089df75f2a
SK-Yeboah <skyeboah9@gmail.com>
2024-03-29 05:31:41 -1200

    write to a file

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,34 +1,21 @@
 #!/usr/bin/node
-const request = require('request');
 
-
-if (process.argv.length !== 3) {
-  console.error('Usage: node 6-completed_tasks.js <API_URL>');
-  process.exit(1); 
-}
-
-const apiUrl = process.argv[2];
-
-
-request(apiUrl, (error, response, body) => {
+const req = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/';
+req.get(url + id, function (error, res, body) {
   if (error) {
-    console.error(error); 
-    return;
+    console.log(error);
   }
-  
-  if (response.statusCode !== 200) {
-    console.error(`Failed to fetch data. Status code: ${response.statusCode}`);
-    return;
+  const data = JSON.parse(body);
+  const dd = data.characters;
+  for (const i of dd) {
+    req.get(i, function (error, res, body1) {
+      if (error) {
+        console.log(error);
+      }
+      const data1 = JSON.parse(body1);
+      console.log(data1.name);
+    });
   }
-  
-  const todos = JSON.parse(body);
-  
-  const completedTasksByUser = todos.reduce((completedTasks, todo) => {
-    if (todo.completed) {
-      completedTasks[todo.userId] = (completedTasks[todo.userId] || 0) + 1;
-    }
-    return completedTasks;
-  }, {});
-  
-  console.log(completedTasksByUser); 
 });
